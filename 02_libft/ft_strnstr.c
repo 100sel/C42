1c7ddb0e3f1460a3d44882339c72b9d3e218caff
100sel <romain.peturaud@gmail.com>
2023-10-13 13:23:38 +0200

    fix divers

--- 02_libft/ft_strnstr.c
+++ 02_libft/ft_strnstr.c
@@ -14,12 +14,14 @@ char *ft_strnstr(const char *s1, const char *s2, size_t n)
         if (s1[i] == s2[j])
         {
             k = i;
-    
-            while (s1[k++] == s2[j++])
-            {}
 
-            if (!s2[j])
-                return (char *)(s1 + i);
+   
+            while (s1[k++] == s2[j++])
+            {
+                if (!s2[j])
+                    return (char *)(s1 + i);
+            }
+
             k = 0;
             j = 0;
         }
