3a84bd931fa66370e040c93d18a801d5e6a9c8d5
100sel <romain.peturaud@gmail.com>
2023-10-13 12:47:52 +0200

    fix divers

--- 02_libft/ft_strlcat.c
+++ 02_libft/ft_strlcat.c
@@ -2,7 +2,7 @@
 
 size_t ft_strlcat(char *dest, const char *src, size_t n)
 {
-    if (n <= sizeof dest + 1)
+    if (n <= ft_strlen(dest) + 1)
         return ft_strlen(dest) + ft_strlen(src);
 
     int     i;
