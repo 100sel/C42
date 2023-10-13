d8b60894917d59e2a73936d6903a7b34066d816f
100sel <romain.peturaud@gmail.com>
2023-10-13 13:49:50 +0200

    fix divers

--- 02_libft/ft_atoi.c
+++ 02_libft/ft_atoi.c
@@ -4,11 +4,11 @@ int power(int n, int p)
 {
     int k;
     int ret;
-    k   = 0;
+    k = 0;
     ret = 1;
     while (k++ < p)
         ret *= n;
-    return ret;    
+    return ret;
 };
 
 int ft_atoi(const char *s)
@@ -18,23 +18,24 @@ int ft_atoi(const char *s)
     int i;
     int j;
 
-    size    = ft_strlen(s);
-    res     = 0;
-    i       = 0;
-    j       = size;
+    size = ft_strlen(s);
+    res = 0;
+    i = 0;
+    j = size;
+
+    if (!ft_isdigit(s[0]) && (s[0] != *"-" || s[0] != *"+"))
+        return 0;
 
     while (i <= size)
     {
-        if ( !ft_isdigit(s[0]) && ( s[0] != *"-" || s[0] != *"+") )
-            return 0;
-
-        if ( i > 0 && !ft_isdigit(s[i]) )
+        if (i > 0 && !ft_isdigit(s[i]))
             return 0;
 
         res += (int)(s[i++]) * (power(10, j));
-        
-        if (s[0] == *"-")
-            res *= -1;
     }
+
+    if (s[0] == *"-")
+        res *= -1;
+
     return res;
 }
