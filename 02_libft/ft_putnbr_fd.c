2b96679db1d9a2f13fbdf40838018b47a5e97ba7
100sel <romain.peturaud@gmail.com>
2023-10-13 17:18:41 +0200

    fix divers

--- 02_libft/ft_putnbr_fd.c
+++ 02_libft/ft_putnbr_fd.c
@@ -2,11 +2,8 @@
 
 void ft_putnbr_fd(int n, int fd)
 {
-    int i; 
-    i = 0;
     char *nbr = ft_itoa(n);
-    ft_putstr_fd(nbr, fd);
-    while (nbr[i])
-        free(nbr + i++); 
+    write(fd, nbr, ft_strlen(nbr));
+    free(nbr); 
     return;
 }
