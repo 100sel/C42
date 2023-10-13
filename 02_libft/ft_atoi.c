9b7e85ee47a8b1550de4ca7c27faa213c985bab1
100sel <romain.peturaud@gmail.com>
2023-10-13 13:47:17 +0200

    fix divers

--- 02_libft/ft_atoi.c
+++ 02_libft/ft_atoi.c
@@ -24,7 +24,17 @@ int ft_atoi(const char *s)
     j       = size;
 
     while (i <= size)
+    {
+        if ( !ft_isdigit(s[0]) && ( s[0] != *"-" || s[0] != *"+") )
+            return 0;
+
+        if ( i > 0 && !ft_isdigit(s[i]) )
+            return 0;
+
         res += (int)(s[i++]) * (power(10, j));
-    
+        
+        if (s[0] == *"-")
+            res *= -1;
+    }
     return res;
 }
