1460db1538a2e71134f1c9d5c8628fda5a7c3d1b
100sel <romain.peturaud@gmail.com>
2023-10-13 17:54:37 +0200

    fix divers

--- 02_libft/ft_putendl_fd.c
+++ 02_libft/ft_putendl_fd.c
@@ -2,7 +2,7 @@
 
 void ft_putendl_fd(char *s, int fd)
 {
-    write(fd, s, ft_strlen(s));
+    write(fd, s, ft_strlen(s) + 1);
     write(fd, "\n", 1);
     return;
 }
--- 02_libft/ft_putstr_fd.c
+++ 02_libft/ft_putstr_fd.c
@@ -2,6 +2,6 @@
 
 void ft_putstr_fd(char *s, int fd)
 {
-    write(fd, s, ft_strlen(s)); 
+    write(fd, s, ft_strlen(s) + 1); 
     return;
 }
