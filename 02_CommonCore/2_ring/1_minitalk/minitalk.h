cc3d94afe51972c70a1d6b2f6eeca91a45c4d6ec
100sel <romain.peturaud@gmail.com>
2023-10-22 23:59:15 +0200

    minitalk done

--- 02_CommonCore/2_ring/1_minitalk/minitalk.h
+++ 02_CommonCore/2_ring/1_minitalk/minitalk.h
@@ -3,4 +3,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
+int is_digit(char *pid);
+int is_print(char *str);
+void sendchar(char c, int pid);
+int getb(char c, int i);
+void sigusr_hdl(int sig, siginfo_t *info, void *ucontext);
---
+++ 02_CommonCore/2_ring/1_minitalk/test.sh
@@ -0,0 +1,5 @@
+i=0
+while [ $i -lt $2 ]; do
+    ./client $1 "Lorizzle sure dolor own yo' amizzle, da bomb adipiscing elit. Nullam things velizzle, sheezy volutpizzle, suscip"
+    i=$((i+1))
+done
