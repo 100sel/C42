4021e98beb9eb64dff4d0b3f04138b2e0d14802e
100sel <romain.peturaud@gmail.com>
2023-10-25 19:11:16 +0200

    ft .h

--- 02_CommonCore/2_ring/2_so_long/so_long.h
+++ 02_CommonCore/2_ring/2_so_long/so_long.h
@@ -8,3 +8,46 @@
 #define HEIGHT          720
 #define NAME            "so_long"
 
+typedef unsigned char byte;
+
+typedef struct s_tile {
+    char type;
+    int x;
+    int y;
+}               t_tile;
+
+typedef struct s_image {
+    void *structure;
+    char *pixels;
+    int  bits_per_pixel;
+    int  line_length;
+    int  endian;
+    int  width;
+    int  height;
+}               t_image;
+
+typedef struct s_data {
+    void    *display;
+    void    *window;
+    t_image *canvas;
+    t_image *floor;
+    t_image *wall;
+    t_image *coin;
+    t_image *player;
+    t_image *exit;
+}               t_data;
+
+t_data          *init_data(void);
+
+void            freeZ(int count, ...);
+int             cleaner(t_data *data);
+
+unsigned int    pixel_get(t_image *img, int x, int y);
+void            pixel_put(t_image *image, int x, int y, unsigned int pixel); 
+int             get_sprite(void *display, char *path, t_image *sprite);
+void            draw_sprite(t_image *canvas, t_image *sprite, 
+        int x_start, int y_start, int width, int height);
+
+int             handler_key(int keysym, t_data *data);
+int             destroy_hdl(t_data *data);
+
