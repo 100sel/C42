b2ecb0917bfc8d716d817be29038955516c68d3e
100sel <romain.peturaud@gmail.com>
2023-10-28 21:10:50 +0200

    init pipex

--- 02_CommonCore/2_ring/1_minitalk/test.sh
+++
@@ -1,5 +0,0 @@
-i=0
-while [ $i -lt $2 ]; do
-    ./client $1 "Lorizzle sure dolor own yo' amizzle, da bomb adipiscing elit. Nullam things velizzle, sheezy volutpizzle, suscip"
-    i=$((i+1))
-done
