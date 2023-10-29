b1b5d9903865bfc09bd6a70d68a55d56b55fb811
100sel <romain.peturaud@gmail.com>
2023-10-29 19:10:03 +0100

    done, kinda dirty but it works

--- 02_CommonCore/2_ring/3_pipex/main.c
+++ 02_CommonCore/2_ring/3_pipex/main.c
@@ -1,10 +1,14 @@
 #include <unistd.h>
 #include <stdlib.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stddef.h>
+#include <string.h>
 #include <wait.h>
 
-#define ERR_ 1
+#define ERR_        1
+#define MAX_ARGS    8
+#define MAX_CHAR    16
 
 void err_hdl(int err_num, char *err_msg)
 {
@@ -12,14 +16,90 @@ void err_hdl(int err_num, char *err_msg)
     exit(err_num);
 }
 
+char *parse_path(char *cmd)
+{
+    char *path = "/usr/bin/";
+    char *path_ptr = path;
+    char *cmd_ptr = cmd;
+    int size_path = (int)strlen(path);
+    int size_cmd = (int)strlen(cmd);
+    int size_t = size_path + size_cmd;
+    char *fp = malloc(size_t + 1);
+    int i = 0;;
+
+    while (i < size_path)
+        fp[i++] = *path_ptr++;
+
+    while (i < size_t)
+        fp[i++] = *cmd_ptr++;
+
+    fp[i] = '\0';
+
+    return fp;
+}
+
+char **split_cmd(char *cmd)
+{
+    char    **args          = calloc(MAX_ARGS, sizeof(char));
+    char    **args_ptr      = args;
+    char    tmp[MAX_CHAR]   = {'\0'};
+    char    *tmp_ptr        = tmp;
+    int     flag            = 0;
+    int     next            = 1;
+    int     size            = (int)strlen(cmd);
+
+    for (int i = 0; i < size; i++)
+    {
+        if (cmd[i] != ' ' && !flag && next && i != size - 1)
+        {
+            flag = 1;
+            next = 0;
+            *tmp_ptr++ = cmd[i];
+            continue;
+        }
+
+        if (cmd[i] != ' ' && flag && !next && i != size - 1)
+        {
+            *tmp_ptr++ = cmd[i];
+            continue;
+        }
+
+        if (cmd[i] == ' ' || i == size - 1)
+        {
+            if (i == size - 1)
+                *tmp_ptr = cmd[i];
+
+            next = 1;
+            flag = 0;
+            *args_ptr = calloc(strlen(tmp) + 1, sizeof(char));
+            
+            for (int j = 0; j < (int)strlen(tmp); j++)
+                (*args_ptr)[j] = tmp[j];
+
+            args_ptr++;
+            tmp_ptr = tmp;
+            memset(tmp, '\0', MAX_CHAR);
+
+            continue;
+        }
+    }
+
+    return args;
+}
+
 int main(int argc, char **argv)
 {
 
     if (argc < 5)
         err_hdl(ERR_, "Error <infile cmd1 cmd2 outfile>");
+
     int pipex[2];
     int a_pid;
     int b_pid;
+    int a_fd;
+    int b_fd;
+    char **args;
+    char *path;
 
     if (pipe(pipex) == -1)
         err_hdl(ERR_, "Error with da pipe opening");
@@ -31,13 +111,21 @@ int main(int argc, char **argv)
     if (!a_pid)
     {
         close(pipex[0]);
+        a_fd = open(argv[1], O_RDONLY);
+        if (a_fd == -1)
+            err_hdl(ERR_, "Error with file1");
 
+        args = split_cmd(argv[2]);
+        path = parse_path(args[0]);
+
+        dup2(a_fd, STDIN_FILENO);
         dup2(pipex[1], STDOUT_FILENO);
         close(pipex[1]);
-
-        printf("exec %s\n", argv[2]);
-//        perror("exec");
-//        err_hdl(ERR_, "Error with 1st cmd");
+        
+        execve(path, args, NULL);
+        
+        perror("exec");
+        err_hdl(ERR_, "Error with 1st cmd");
     }
 
     if (a_pid)
@@ -48,20 +136,30 @@ int main(int argc, char **argv)
     if (!b_pid)
     {
         close(pipex[1]);
+        b_fd = (open(argv[4], O_WRONLY));
+        if(b_fd == -1)
+            err_hdl(ERR_, "Error with file2");
+
+        args = split_cmd(argv[3]);
+        path = parse_path(args[0]);
 
         dup2(pipex[0], STDIN_FILENO);
+        dup2(b_fd, STDOUT_FILENO);
         close(pipex[0]);
 
-        printf("exec %s\n", argv[3]);
-//        perror("exec");
-//        err_hdl(ERR_, "Error with 2nd cmd");
+        execve(path, args, NULL);
+        
+        perror("exec");
+        err_hdl(ERR_, "Error with 2nd cmd");
     }
 
     close(pipex[0]);
     close(pipex[1]);
+    close(a_fd);
+    close(b_fd);
 
-    wait(NULL);
-    wait(NULL);
+    waitpid(a_pid, NULL, 0);
+    waitpid(b_pid, NULL, 0);
 
     return 0;
 }
